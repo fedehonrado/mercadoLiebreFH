d9219eccaa88a382d214cf571cdce22df881fc22
fedehonrado <fede.honrado@gmail.com>
2021-11-05 20:37:58 -0300

    Subiendo ML a repositorio remoto

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const path = require('path')
+
+
+
+app.get('/', function (request, response) {
+    response.sendFile(path.join(__dirname, 'views/index.html'))
+})
+
+app.use(express.static('public'))
+app.use(express.static('views'))
+
+app.get('*', function (request, response) {
+    response.send('NOT FOUND', 404)
+})
+
+app.listen(port, function () {
+    console.log('Servido corriendo en corriendo en el puerto ' + port);
+})
+
